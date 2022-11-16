8f3a3a53fc091f99688b40352065da2814cee09b
Alexandr <guk.zverolis@yandex.ru>
2022-11-16 14:15:31 +0300

    result commit

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,3 +1,60 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите несколько целых чисел: ");
+string text = Console.ReadLine();
+int size = 1;
+int j = 0;
+string record = String.Empty;
+
+for (int i = 0; i < text.Length; i++)
+{
+    int count = Convert.ToInt32(text[i]);
+    if(count == 44)
+    {
+        size++;
+    } 
+}
+
+int [] array = new int[size];
+
+for (int i = 0; i < text.Length; i++)
+{
+    int num = Convert.ToInt32(text[i]);
+   
+    if(num == 45 || num >= 48 && num <= 57)
+    {
+    string str = Convert.ToString(text[i]);
+    record = record + str;
+    }
+    if(num == 44 || i == text.Length - 1)
+    {
+    int number = Convert.ToInt32(record);
+    record = string.Empty;
+    array[j] = number;
+    j++;
+    }
+}
+WriteArray(array);
+PositivNumbers(array);
+
+void WriteArray(int[] number)
+{
+    for(int i = 0; i < (number.Length - 1); i++)
+    {
+        Console.Write($"{number[i]}, ");
+    }
+    Console.WriteLine($"{number[number.Length - 1]} ");
+}
+
+void PositivNumbers(int[] numbers)
+{
+    int count = 0;
+    for(int i = 0; i < numbers.Length; i++)
+    if(numbers[i] > 0)
+    {
+        count++;
+    }
+    Console.WriteLine($"Количество положительных чисел: {count}");
+}
--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -2,3 +2,24 @@
 // уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+
+Console.WriteLine("Введите коэффициенты линейных функций: ");
+double k1 = ReadDub("k1 = ");
+double b1 = ReadDub("b1 = ");
+double k2 = ReadDub("k2 = ");
+double b2 = ReadDub("b2 = ");
+
+if( k1 == k2)
+{
+    Console.WriteLine("Функции не пересекаются");
+}
+double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+double y = Math.Round((k1 * x + b1), 2);
+
+Console.Write($"x = {x}  y = {y} ");
+
+double ReadDub(string message)
+{
+    Console.Write(message);
+    return Math.Round(Convert.ToDouble(Console.ReadLine()), 2);
+}
